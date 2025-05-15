a7cb294f9754ace00bb0398f762dc9cb32c85690
crystal <crystal@crystaldeMacBook-Pro.local>
2025-05-15 12:30:37 +0100

    test

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,4 +1,18 @@
 ﻿const generatedBibEntries = {
+    "Ahlers2014Replicable": {
+        "abstract": "Monitoring a computer network's security state is a di cult task as network components rarely share their information. The IF-MAP speci fication de fines a client/server-based protocol that enables network components to share security information among each other, which is represented in a graph structure. Visualization of this data is challenging due to the highly dynamic topology and the mapping of logical nodes onto physical devices. Furthermore, data in a MAP server is volatile and there is no standardized way to preserve and review changes or previous states of a MAP graph. The evolution of such a graph, however, embodies valuable information for the analysis of past incidents and attacks on the network infrastructure. In this paper we introduce a software framework to visualize MAP data and propose a solution for the effi cient long-term storage and replication of MAP graphs. We demonstrate how changes in the graph structure between given points in time can be computed and visualized.",
+        "author": "Ahlers, Volker and Heine, Felix and Hellmann, Bastian and Kleiner, Carsten and Renners, Leonard and Rossow, Thomas and Steuerwald, Ralf",
+        "booktitle": "Joint Proceedings of the Fourth International Workshop on Euler Diagrams and the First International Workshop on Graph Visualization in Practice",
+        "keywords": "type:application, application:infrastructure, paradigm:node-link, time:animation, graph_difference, evaluation:none",
+        "pages": "32--41",
+        "publisher": "CEUR-WS.org",
+        "references": "Federico2011Visual Dutot2007GraphStream",
+        "series": "GraphViP",
+        "title": "Replicable Security Monitoring: Visualizing Time-Variant Graphs of Network Metadata",
+        "type": "inproceedings",
+        "url": "http://ceur-ws.org/Vol-1244/GViP-paper1.pdf",
+        "year": "2014"
+    },
     "Beck2016Visual": {
         "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
         "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
